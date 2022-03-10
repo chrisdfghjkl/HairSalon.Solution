4a463b6b2953b1c147eec4a6382ea23e8107b2d3
chrisdfghjkl <cdepastene@gmail.com>
2022-03-10 15:47:57 -0800

    add business logic

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,6 +4,16 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-    
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+
   }
 }
