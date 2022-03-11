faede503bff10db11a9b37495f337936eccb37c2
chrisdfghjkl <cdepastene@gmail.com>
2022-03-10 16:10:19 -0800

    add index/root route

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace HairSalon.Controllers
 {
   public class HomeController : Controllers
   {
-    
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
